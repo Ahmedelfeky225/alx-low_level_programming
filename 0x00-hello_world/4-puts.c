93a8a8ebf0a9672ebd26f82658d7d4d5a6fb70a8
Ahmed <elfekyahmed337@gmail.com>
2023-03-16 18:20:11 -0700

    my third commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
-int main(void){
+/**
+ * main Entry point
+ *
+ * Description: A C Pogram that prints with put function
+ *
+ * Return: Always 0 (success)
+ */
+int main(void) 
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
