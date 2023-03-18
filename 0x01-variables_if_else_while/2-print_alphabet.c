3ef11caab57c5f9ec5869025f1ffdd3347d5e493
Ahmed <elfekyahmed337@gmail.com>
2023-03-17 18:14:59 -0700

    my third commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,12 +9,11 @@
 int main(void)
 {
 	char low;
-	char upper;
 
 	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
-	for (upper = 'A'; upper <= 'Z'; ++upper)
-		putchar(upper);
+	for (low = 'A'; low <= 'Z'; ++low)
+		putchar(low);
 		putchar('\n');
 
 	return (0);
