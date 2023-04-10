183f1330e7b4004551d55f866c34e7f5bc3c95dc
Ahmed <elfekyahmed337@gmail.com>
2023-04-10 13:39:59 -0700

    my third commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Description: prints the name of the function
+ *
+ *@argc: amount of arguments passed through the program
+ *@argv: pointer that contains the arrays of the arguments
+ *
+ *Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("s\n", arg[0]);
+	return (0);
+}
